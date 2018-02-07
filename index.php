b329030aa4e936706a561c765410b1f74c63c325
jm <johnmichaelmaytona@gmail.com>
2018-02-06 20:18:57 -0800

    Variables and Concatinate

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*Variable Rules
+    -Prefix $
+    -Starts with letters and underscore only
+    -Case Sensitive*/
+
+/*$outPut = 'We are one!';
+echo $outPut;*/
+
+/*Adding two integer*/
+
+/*$num1 = 10;
+$num2 = 4;
+$sum = $num1 + $num2;
+
+echo $sum;*/
+
+$word1 = 'Hello';
+$word2 = 'World';
+$lastWord = $word1 . $word2;
+$spacedWord = $word1 .' '. $word2;
+$doubleQoute = "$word1 $word2";
+
+echo $lastWord;
+echo $spacedWord;
+echo $doubleQoute;
+
+?>
